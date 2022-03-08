851e9bd36d2d4625337fe71cc97a44a6b5f2cd5b
Shruthi Kantheti <kantheti.shruthi@gmail.com>
2022-03-08 11:32:55 -0600

    Creating custom Context

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {},
+    onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+    if(storedUserLoggedInInformation === 1) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', 1);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+  };
+
+  return (
+      <AuthContext.Provider value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler
+      }}>
+          {props.children}
+      </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
